e80c2187551da08badc9af62c2408a5afd0f4a25
DiogoS5 <diogomppsousa@gmail.com>
2023-10-21 17:36:38 +0100

    mode1 seperate function

--- main.c
+++ main.c
@@ -6,10 +6,85 @@
 #include "stack.h"
 #include "list.h"
 #include "gravity.h"
+
+void mode1(FILE* outfp, int** tiles, int lines, int columns){
+    int reset = 1, plays = 0, score = 0, counter = 0, value = 0;
+    node* stack_head = NULL, *groups_head = NULL, *groups_curr = NULL;
+
+    while(reset){
+        reset = 0;
+        for(int line = 0; line < lines; line++){
+            for(int column = 0; column < columns; column++){     
+                counter = 0; //initialize tile counter
+                value = tiles[line][column];
+
+                if(value > 0){ //search for group
+
+                    stackPush(&stack_head, line, column);
+                    while(stack_head != NULL){
+                        int l = stack_head->l;
+                        int c = stack_head->c;
+
+                        stackPop(&stack_head);
+                        if (tiles[l][c] == value){
+                            tiles[l][c] = -1;
+                            counter += 1;
+
+                            if(l + 1 < lines){
+                                stackPush(&stack_head, l + 1, c);
+                            }
+                            if(l - 1 >= 0){
+                                stackPush(&stack_head, l - 1, c);
+                            }
+                            if(c + 1 < columns){
+                                stackPush(&stack_head, l, c + 1);
+                            }
+                            if(c - 1 >= 0){
+                                stackPush(&stack_head, l, c - 1);
+                            }
+                        }
+                    }
+                }
+                else{ //not a colour
+                    continue;
+                }
+
+                if(counter == 1){ //only one tile
+                    tiles[line][column] = value; //return tile to original value
+                    continue;
+                }
+                //there was a group
+                if(groups_head == NULL){
+                    groups_head = addToList(groups_curr, line, column);
+                    groups_curr = groups_head;
+                }
+                else{
+                    groups_curr = addToList(groups_curr, line, column);
+                }
+                
+                tiles = verticalGravity(lines, columns, tiles);
+                tiles = horizontalGravity(lines, columns, tiles);
+
+                score += counter*(counter-1); //print score
+                plays += 1;
+
+                reset = 1;
+                break;
+            }
+            if(reset){
+                break;
+            }
+        }
+    }
+
+    fprintf(outfp, "%d %d\n", plays, score);
+    printList(outfp, groups_head); 
+    deleteList(groups_head); 
+
+}
+
 int main(int argc, char *argv[]) {
     int lines, columns, mode;
-    int reset, plays, score = 0, counter = 0, value = 0;
-    node* stack_head = NULL, *groups_head = NULL, *groups_curr = NULL;
     FILE* fp, *outfp;
 
     if (argc < 2) {//there must be a file name
@@ -66,82 +141,7 @@ int main(int argc, char *argv[]) {
 
         tiles = readTiles(fp, lines, columns, tiles);
         
-        reset = 1;
-        plays = 0;
-        score = 0;
-        groups_head = NULL;
-        groups_curr = NULL;
-
-        while(reset){
-            reset = 0;
-            for(int line = 0; line < lines; line++){
-                for(int column = 0; column < columns; column++){     
-                    counter = 0; //initialize tile counter
-                    value = tiles[line][column];
-
-                    if(value > 0){ //search for group
-
-                        stackPush(&stack_head, line, column);
-                        while(stack_head != NULL){
-                            int l = stack_head->l;
-                            int c = stack_head->c;
-
-                            stackPop(&stack_head);
-                            if (tiles[l][c] == value){
-                                tiles[l][c] = -1;
-                                counter += 1;
-
-                                if(l + 1 < lines){
-                                    stackPush(&stack_head, l + 1, c);
-                                }
-                                if(l - 1 >= 0){
-                                    stackPush(&stack_head, l - 1, c);
-                                }
-                                if(c + 1 < columns){
-                                    stackPush(&stack_head, l, c + 1);
-                                }
-                                if(c - 1 >= 0){
-                                    stackPush(&stack_head, l, c - 1);
-                                }
-                            }
-                        }
-                    }
-                    else{ //not a colour
-                        continue;
-                    }
-
-                    if(counter == 1){ //only one tile
-                        tiles[line][column] = value; //return tile to original value
-                        continue;
-                    }
-                    //there was a group
-                    if(groups_head == NULL){
-                        groups_head = addToList(groups_curr, line, column);
-                        groups_curr = groups_head;
-                    }
-                    else{
-                        groups_curr = addToList(groups_curr, line, column);
-                    }
-                    
-                    tiles = verticalGravity(lines, columns, tiles);
-                    tiles = horizontalGravity(lines, columns, tiles);
-
-                    score += counter*(counter-1); //print score
-                    plays += 1;
-
-                    reset = 1;
-                    break;
-                }
-                if(reset){
-                    break;
-                }
-            }
-        }
-
-        fprintf(outfp, "%d %d\n", plays, score);
-        printList(outfp, groups_head); 
-        deleteList(groups_head); 
-        //outputTiles(outfp, lines, columns, tiles);   
+        mode1(outfp, tiles, lines, columns);
             
         //Frees
         for (int l = 0; l < lines; l++) {
